f2ebfa08beaf39fbe52e903c64f9f1e3a246a133
CRO <omalley_90@hotmail.com>
2025-12-23 14:29:26 -0600

    Update practice.py

--- Practice/07A_Basic_For_Loops/practice.py
+++ Practice/07A_Basic_For_Loops/practice.py
@@ -3,19 +3,25 @@
 # ===================================
 
 # Practice 1: Print "Hello" 5 times
-
+for i in range(5):
+    print("Hello")
 
 # Practice 2: Print numbers 0-4 using range(5)
-
+for i in range(5):
+    print(i)
+    
 
 # Practice 3: Print numbers 1-10 using range(1, 11)
-
+for i in range(1, 11):
+    print(i)
 
 # Practice 4: Print even numbers from 0-10 using range(0, 11, 2)
-
+for i in range(0, 10):
+    print(i * 2)
 
 # Practice 5: Loop through a string and print each letter
-
+for i in range("Hello World"):
+    print(i)
 
 # Practice 6: Print numbers 10 down to 1 using range()
 
